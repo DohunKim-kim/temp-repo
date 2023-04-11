cf73ac828b344257db26f61927e68107d27c5deb
DoHunKim <ehgns010305@gmail.com>
2023-04-11 21:28:30 +0900

    미일치 학번 표현 안함

--- week3/register.js
+++ week3/register.js
@@ -54,6 +54,8 @@ function findMajor(){
         major.innerHTML = "에너지신소재화학공학부";
     } else if(str[1] == "80"){
         major.innerHTML = "산업경영학부";
+    } else{
+        major.innerHTML = "";
     }
 }
 
@@ -61,8 +63,4 @@ const autoHyphen = (target) => {
     target.value = target.value
       .replace(/[^0-9]/g, '')
      .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/(\-{1,2})$/g, "");
-}
-
-function moveToLoginPage(){
-    
 }
