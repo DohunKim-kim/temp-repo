34111df3761421cd28758272b6178aaabb126a5d
DoHunKim <ehgns010305@gmail.com>
2023-04-13 13:04:31 +0900

    야구 게임 수정1

--- baseball game/main.js
+++ baseball game/main.js
@@ -1,61 +1,100 @@
-// 야구 게임
-// 룰: 입력받은 숫자의 B: , S: 값을 알려준다.
-// 버튼 누르면 정답값이 결정 됨
+const MAX_RANDOM_NUMBER = 9;
+const MIN_RANDOM_NUMBER = 1;
+const $inputNumber = document.querySelector('.input-number');
+const $gameTable = document.querySelector(".game-table");
 
-let ans;
-function test(val){
-    if(val === "3자리"){
-        ans = 159;
-    } else if(val === "4자리"){
-        ans = 3806;
-    } else if(val === "5자리"){
-        ans = 40186;
-    }
-    console.log(ans);
-}
+let answer;
 
-let i = 1;
-function addVal(){
-    const num = document.querySelector(".num");
-    const gameTable = document.querySelector(".game-table");
-    const newRow = gameTable.insertRow();
-
-    const turn = newRow.insertCell(0);
-    const number = newRow.insertCell(1);
-    const result = newRow.insertCell(2);
-    
-    let val = game(ans)
-    let strAns = ans.toString();
-    
-    turn.innerText = `${i++}`;
-    number.innerText = `${num.value}`
-    if(val[1] === strAns.length){
-        result.innerText = `O.K.`
-    } else{
-        result.innerText = `B: ${val[0]}, S: ${val[1]}`;
+function inputDigit(numberOfDigit){
+    if(numberOfDigit === "3자리"){
+        answer = randomNumber(3);
+    } else if(numberOfDigit === "4자리"){
+        answer = randomNumber(4);
+    } else if(numberOfDigit === "5자리"){
+        answer = randomNumber(5);
     }
 }
 
+function randomNumber(numberOfDigit){
+    let result = 0;
+    let existingNumbers = [];
+    for (step = 0; step < numberOfDigit; step++) {
+        let randomNumber = Math.floor(Math.random() * (9 - 1) + 1);
+        while (existingNumbers.includes(randomNumber)){
+            randomNumber = Math.floor(Math.random() * (9 - 1) + 1);
+        }
+        existingNumbers.push(randomNumber);
+    }
 
-function game(ans){
-    const num = document.querySelector(".num").value;
-    const strAns = ans.toString();
-    const strNum = num.toString();
-    let k = 0;
-    let result = [0, 0];
-    while(k < strAns.length){
-        if(strAns[k] === strNum[k]){
-            result[1] += 1;
+    let weight = 1;
+    for(j = 0; j < numberOfDigit; j++){
+        result += existingNumbers[j] * weight;
+        weight *= 10;
+    }
+
+    console.log(result);
+    return result;
+}
+
+let index = 1;
+function addValue(){
+    const newRow = $gameTable.insertRow();
+    const pitchResult = checkBallAndStrike(answer);
+    const convertedAnswer = answer.toString();
+
+    let isValid = 0;
+    if($inputNumber.value.length === convertedAnswer.length){
+        for(let i = 0; i < $inputNumber.value.length; i++){
+            for(let j = 0; j < $inputNumber.value.length; j++){
+                if(i !== j){
+                    if($inputNumber.value[i] === $inputNumber.value[j]){
+                        isValid = 1;
+                    }
+                }
+            }
+        }
+        if(isValid === 0){
+            const turn = newRow.insertCell(0);
+            const number = newRow.insertCell(1);
+            const result = newRow.insertCell(2);
+            turn.innerText = `${index++}`;
+            number.innerText = `${$inputNumber.value}`
+            if(pitchResult[1] === convertedAnswer.length){
+                result.innerText = `O.K.`
+            } else{
+                result.innerText = `B: ${pitchResult[0]}, S: ${pitchResult[1]}`;
+            }
         } else{
-            for(let j of strAns){
-                if(j === strNum[k]){
-                    result[0] += 1;
+            alert("올바르지 않은 값입니다.");
+        }
+       
+    } else{
+        alert("올바르지 않은 값입니다.");
+    }
+
+}
+
+function checkBallAndStrike(answer){
+    const convertedAnswer = answer.toString();
+    const convertedInputNumber = $inputNumber.value.toString();
+    let k = 0;
+    let score = [0, 0];
+    while(k < convertedAnswer.length){
+        if(convertedAnswer[k] === convertedInputNumber[k]){
+            score[1] += 1;
+        } else{
+            for(let item of convertedAnswer){
+                if(item === convertedInputNumber[k]){
+                    score[0] += 1;
                 }
             }
         }
         k += 1;
     }
-    return result;
+    return score;
 }
 
 
+// 새롭게 구현해야 하는 것들
+// 1. 버튼 누르면 표 초기화 하기
+// 2. 게임이 끝나면 더 이상 입력 받지 않게 하기
